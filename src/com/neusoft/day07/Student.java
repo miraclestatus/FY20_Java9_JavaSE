6654e4dd773841893bfc5910436551f71060aadc
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-21 14:53:05 +0800

    111

---
+++ src/com/neusoft/day07/Student.java
@@ -0,0 +1,19 @@
+package com.neusoft.day07;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/21 14:29
+ */
+public class Student {
+    // 建立了一个 Student类
+    // 成员变量  ---》 事物的属性
+    String name;
+    int age;
+    // 成员方法  ---》 事物的行为
+    public void study(){
+        System.out.println("good good study, day day up");
+    }
+    public void eat(){
+        System.out.println("学习饿了得吃饭");
+    }
+}
---
+++ src/com/neusoft/day07/TestStudent.java
@@ -0,0 +1,31 @@
+package com.neusoft.day07;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/21 14:38
+ */
+public class TestStudent {
+    public static void main(String[] args) {
+        // 类名 对象名 = new 类名();
+        Student student = new Student();
+        // com.neusoft.day07.Student@1b6d3586
+        // 对象如果没有处理，打印出来的格式
+        // 包名 .类名 @ 内存地址
+        System.out.println(student);
+        // 访问类中的成员
+        System.out.println(student.name); //null
+        System.out.println(student.age); // 0
+        // 给成员变量赋值
+        student.name = "李建军";
+        student.age = 20;
+        student.age = 18;
+        System.out.println("++++++++++++++");
+
+        System.out.println(student.name); //null
+        System.out.println(student.age); // 0
+
+        // 调用方法
+        student.eat();
+        student.study();
+    }
+}

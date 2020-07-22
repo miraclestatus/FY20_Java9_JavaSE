9648910c624cf0a6a0eb74c9bb5bfff5ed7ea6dd
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-22 10:07:34 +0800

    1111

---
+++ src/com/neusoft/day08/Demo3.java
@@ -0,0 +1,40 @@
+package com.neusoft.day08;
+
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/22 10:00
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 20 ; i++) {
+            list.add(random.nextInt(1000)+1);
+        }
+        System.out.println("创建的集合是"+ list);
+        ArrayList<Integer> arrayList = getArrayList(list);
+        System.out.println("筛选后的集合是"+ arrayList);
+
+
+    }
+    public static ArrayList<Integer> getArrayList(ArrayList<Integer> list){
+        ArrayList<Integer> oushuList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            Integer number = list.get(i);
+
+            if(number%2 ==0){
+                oushuList.add(number);
+            }
+
+        }
+
+        return oushuList;
+    }
+
+
+}

e86ea9bf7e15581774223d5eca3da06fddeb0ade
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-25 11:37:02 +0800

    1111

--- src/com/neusoft/day11/InnerClassDemo2/InnerDemo2.java
+++ src/com/neusoft/day11/InnerClassDemo2/InnerDemo2.java
@@ -4,23 +4,15 @@ package com.neusoft.day11.InnerClassDemo2;
  * @author Eric Lee
  * @date 2020/7/25 11:14
  */
-public class InnerDemo {
+public class InnerDemo2 {
     public static void main(String[] args) {
 
-        /**
-         * 等号左边：是多态赋值， （抽象类）接口类型引用指向子类对象
-         * 等号右边：是匿名内部类， 定义并创建该接口的子类对象
-         */
-
-        FlyAble f  =  new FlyAble() {
+        showFly(new FlyAble() {
             @Override
             public void fly() {
                 System.out.println("嗷嗷飞");
             }
-        };
-        System.out.println(f);
-
-        showFly(f);
+        });
 
     }
 

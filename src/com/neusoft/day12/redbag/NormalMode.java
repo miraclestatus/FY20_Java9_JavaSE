db768b6c989e7392345133b33991c0e03a7a892c
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-27 09:30:44 +0800

    1111

--- src/com/neusoft/day12/redbag/NormalMode.java
+++ src/com/neusoft/day12/redbag/NormalMode.java
@@ -10,7 +10,18 @@ import java.util.ArrayList;
  */
 public class NormalMode implements OpenMode {
     @Override
-    public ArrayList<Integer> divide(int totalMoney, int totalCount) {
-        return null;
+    public ArrayList<Integer> divide(final int totalMoney, final int totalCount) {
+        ArrayList<Integer> list = new ArrayList<>();
+        // 100/3
+        int avg = totalMoney / totalCount;
+        int mod = totalMoney % totalCount;// 零头
+
+        for (int i = 0; i < totalCount-1 ; i++) {
+            list.add(avg);
+        }
+        // l零头放在最后一个包中
+        list.add(avg+mod);
+
+        return list;
     }
 }

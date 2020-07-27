324bc3ce4e5f380d68bd27e0d030634ca4c3182d
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-27 09:33:28 +0800

    111

--- src/com/neusoft/day12/redbag/Bootstrap.java
+++ src/com/neusoft/day12/redbag/Bootstrap.java
@@ -4,9 +4,21 @@ package com.neusoft.day12.redbag;
  * @author Eric Lee
  * @date 2020/7/27 08:58]
  * 红包界面启动程序
+ *
+ *  普通红包： 之前介绍过
+ *
+ *
  */
 public class Bootstrap {
     public static void main(String[] args) {
+
+        // 设置标题
+
+        // 设置群主名
+
+
+        //  设置分发策略
+
 
     }
 }

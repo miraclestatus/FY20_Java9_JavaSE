f90dd01041d981cd18ff31539c87f508b95d6f18
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-27 09:23:54 +0800

    1111

---
+++ src/com/neusoft/day12/redbag/NormalMode.java
@@ -0,0 +1,16 @@
+package com.neusoft.day12.redbag;
+
+import com.neusoft.day12.redbag.utils.OpenMode;
+
+import java.util.ArrayList;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/27 09:20
+ */
+public class NormalMode implements OpenMode {
+    @Override
+    public ArrayList<Integer> divide(int totalMoney, int totalCount) {
+        return null;
+    }
+}

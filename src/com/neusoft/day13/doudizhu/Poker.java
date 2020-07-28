1eb8ec50e0b1bb8af5d82745e8300618fe46e2d4
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-28 16:50:04 +0800

    111

--- src/com/neusoft/day13/doudizhu/Poker.java
+++ src/com/neusoft/day13/doudizhu/Poker.java
@@ -47,9 +47,38 @@ public class Poker {
         }
         pokerBox.add("大王");
         pokerBox.add("小王");
-//        System.out.println(pokerBox);
-        // 洗牌
+        //  System.out.println(pokerBox);
+        // 洗牌: Collections 工具类 都是静态方法
+        // shuffle() 使用默认随机元对指定列表进行置换
         Collections.shuffle(pokerBox);
+        //   System.out.println(pokerBox);
+        // 发牌 创建player1、player2、player3、dipai底牌4个集合
+        ArrayList<String> player1 = new ArrayList<>();
+        ArrayList<String> player2 = new ArrayList<>();
+        ArrayList<String> player3 = new ArrayList<>();
+        ArrayList<String> dipai = new ArrayList<>();
+        for (int i = 0; i < pokerBox.size(); i++) {
+            String card = pokerBox.get(i);
+            // 判断i值，先判断
+            if(i >= 51){
+                dipai.add(card);
+            }else {
+                if(i%3==0){
+                   player1.add(card);
+                }else if (i%3==1){
+                    player2.add(card);
+                }else {
+                    player3.add(card);
+
+                }
+            }
+        }
+
+        // look look
+        System.out.println("王思聪的牌"+ player1);
+        System.out.println("马云的牌"+ player2);
+        System.out.println("我的牌"+ player3);
+        System.out.println("底牌"+ dipai);
 
     }
 

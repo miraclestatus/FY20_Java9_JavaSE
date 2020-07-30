581e98ab8ab1aa9c41d7855021296b3b5b5144d1
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-30 15:21:58 +0800

    111

---
+++ src/com/neusoft/day15/lambdaDemo/Demo3.java
@@ -0,0 +1,39 @@
+package com.neusoft.day15.lambdaDemo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/30 15:02
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        Person[] array = {
+            new Person("貂蝉", 223),
+            new Person("孙尚香", 18),
+            new Person("妲己", 300),
+            new Person("杨玉环", 221),
+        };
+        Comparator<Person> comp =  new Comparator<>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return false;
+            }
+        };
+        // 
+        Arrays.sort(array, comp);
+
+        for(Person person : array){
+            System.out.println(person);
+        }
+
+    }
+//    new Comparator
+
+}

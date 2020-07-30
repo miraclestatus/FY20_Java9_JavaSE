3e5feedb27f7abf1fed59c8289f2862d88e18fb2
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-07-30 15:02:18 +0800

    111

---
+++ src/com/neusoft/day15/lambdaDemo/Person.java
@@ -0,0 +1,36 @@
+package com.neusoft.day15.lambdaDemo;
+
+import java.lang.ref.PhantomReference;
+
+/**
+ * @author Eric Lee
+ * @date 2020/7/30 15:01
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

c7c2141a4b1d0effdeeca3ff14d3a564c98a8715
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-05 10:07:49 +0800

    11111

--- src/com/neusoft/jdbc/empselectDemo/JDBCEmp.java
+++ src/com/neusoft/jdbc/empselectDemo/JDBCEmp.java
@@ -2,6 +2,8 @@ package com.neusoft.jdbc.empselectDemo;
 
 import com.neusoft.jdbc.empselectDemo.domain.Emp;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,8 +16,66 @@ public class JDBCEmp {
         new JDBCEmp().findAll();
 //        findAll();
     }
-    public List<Emp>  findAll(){
-        
-       return null;
+
+    public List<Emp> findAll() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Emp>  list = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/java9", "root", "root");
+
+            String sql = "select * from account;";
+            stmt = conn.createStatement();
+            // 封装查询结果
+            rs = stmt.executeQuery(sql);
+
+            list = new ArrayList<>();
+
+            while (rs.next()) {
+                // TODO
+
+
+                Emp emp = new Emp();
+               list.add(emp);
+            }
+
+
+
+
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+        return list;
     }
 }
